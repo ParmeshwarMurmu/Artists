0d0ac789a99df374e7f10ba9f6f7ef091ae06f56
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2023-12-01 21:23:32 +0530

    Artist

--- frontend/src/Components/JoinDrawer.jsx
+++ frontend/src/Components/JoinDrawer.jsx
@@ -1,20 +1,28 @@
 import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, FormControl, FormLabel, IconButton, Input, InputGroup, InputRightElement, Text, Tooltip, useDisclosure } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import style from '../CSS/Navbar.module.css'
 import { FaUserLarge } from "react-icons/fa6";
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import artVideo from '../Assets/Artist_Video.mp4'
 import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+import { registerFirstNameAction, registerConfirmPasswordAction, registerEmailAction, registerLastNameAction, registerPasswordAction } from '../Redux/UserRegisterReducer/action';
+// import { registerConfirmPasswordAction, registerEmailAction, registerFirstNameAction, registerLastNameAction, registerPasswordAction } from '../Redux/UserRegistrationReducer/action';
 export const JoinDrawer = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const btnRef = React.useRef()
     const [showPassword, setShowPassword] = useState(false)
     const [showPasswordConfirm, setShowPasswordConfirm] = useState(false)
-    const dipatch = useDispatch()
+    const dispatch = useDispatch()
+    const ref = useRef(null)
+    const [verify, setVerify] = useState("")
     
     const {firstName, lastName, email, password, confirmPassword} = useSelector((store)=>{
         return {
-            firstName: store.UserRegisterReducer.firstName
+            firstName: store.UserRegistrationReducer.firstName,
+            lastName: store.UserRegistrationReducer.lastName,
+            email: store.UserRegistrationReducer.email,
+            password: store.UserRegistrationReducer.password,
+            confirmPassword: store.UserRegistrationReducer.confirmPassword,
         }
     }, shallowEqual)
 
@@ -22,9 +30,78 @@ export const JoinDrawer = () => {
         setShowPassword(!showPassword);
     };
 
-    const confirmPassword = () => {
+    const confirmPasswordToggle = () => {
         setShowPasswordConfirm(!showPasswordConfirm)
     }
+
+    const checkPasswordStrength = (pass)=>{
+        const alpha = "abcdefghijklmnopqrstuvwxyz"
+        const ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const numeric = "0123456789";
+        const specialChar = "!`@#$%^&*()_+=-{}[]|,.<>?/";
+        if(pass.length >= 6){
+            let hasLowercase = false;
+            let hasUppercase = false;
+            let hasNumeric = false;
+            let hasSpecialChar = false;
+        
+            for (let i = 0; i < pass.length; i++) {
+                const char = pass[i];
+        
+                if (alpha.includes(char)) {
+                    hasLowercase = true;
+                } else if (ALPHA.includes(char)) {
+                    hasUppercase = true;
+                } else if (numeric.includes(char)) {
+                    hasNumeric = true;
+                } else if (specialChar.includes(char)) {
+                    hasSpecialChar = true;
+                }
+            }
+        
+            // Check for lowercase letters
+            if (!hasLowercase) {
+                return "Password should contain at least one lowercase letter";
+            }
+        
+            // Check for uppercase letters
+            if (!hasUppercase) {
+                return "Password should contain at least one uppercase letter";
+            }
+        
+            // Check for numeric digits
+            if (!hasNumeric) {
+                return "Password should contain at least one numeric digit";
+            }
+        
+            // Check for special characters
+            if (!hasSpecialChar) {
+                return "Password should contain at least one special character";
+            }
+        
+            // Password meets all criteria
+            return "OK"; 
+        }
+
+    }
+
+    const RegisterUserHandler = ()=>{
+
+        const result = checkPasswordStrength(password)
+        // console.log("result", result);
+        if(result!=='OK'){
+        //    ref.current = result;
+          setVerify(result)
+        }
+
+        console.log(ref.current)
+    
+
+    }
+
+    
+
+
 
     
 
@@ -61,19 +138,21 @@ export const JoinDrawer = () => {
                         </div>
                         <FormControl isRequired>
                             <FormLabel>First Name</FormLabel>
-                            <Input placeholder='First name' />
+                            <Input placeholder='First name' value={firstName} onChange={(e)=>{dispatch(registerFirstNameAction(e.target.value))}}  />
 
                             <FormLabel>Last Name</FormLabel>
-                            <Input placeholder='First name' />
+                            <Input placeholder='Last name' value={lastName} onChange={(e)=>{dispatch(registerLastNameAction(e.target.value))}} />
 
                             <FormLabel>Email</FormLabel>
-                            <Input placeholder='First name' />
+                            <Input placeholder='Email' value={email} onChange={(e)=>{dispatch(registerEmailAction(e.target.value))}} />
 
                             <FormLabel>Password</FormLabel>
                             <InputGroup>
                                 <Input
                                     type={showPassword ? 'text' : 'password'}
                                     placeholder="Password"
+                                    value={password}
+                                    onChange={(e)=>{dispatch(registerPasswordAction(e.target.value))}}
                                 />
                                 <InputRightElement width="4.5rem">
                                     <IconButton
@@ -82,6 +161,7 @@ export const JoinDrawer = () => {
                                         size="sm"
                                         onClick={handleTogglePassword}
                                         icon={showPassword ? <FaEyeSlash /> : <FaEye />}
+                                        
                                     />
                                 </InputRightElement>
                             </InputGroup>
@@ -91,14 +171,17 @@ export const JoinDrawer = () => {
                             <InputGroup>
                                 <Input
                                     type={showPasswordConfirm ? 'text' : 'password'}
-                                    placeholder="Password"
+                                    placeholder="Confirm Password"
+                                    value={confirmPassword}
+                                    onChange={(e)=>{dispatch(registerConfirmPasswordAction(e.target.value))}}
                                 />
+                              <Text>{verify}</Text>
                                 <InputRightElement width="4.5rem">
                                     <IconButton
                                         variant={'none'}
                                         h="1.75rem"
                                         size="sm"
-                                        onClick={confirmPassword}
+                                        onClick={confirmPasswordToggle}
                                         icon={showPasswordConfirm ? <FaEyeSlash /> : <FaEye />}
                                     />
                                 </InputRightElement>
@@ -114,7 +197,7 @@ export const JoinDrawer = () => {
                         <Button className={style.negativeBtn} variant='none' mr={3} onClick={onClose}>
                             Cancel
                         </Button>
-                        <Button className={style.submitBtn} variant={'none'}>Register</Button>
+                        <Button className={style.submitBtn} variant={'none'} onClick={RegisterUserHandler}>Register</Button>
                     </DrawerFooter>
                 </DrawerContent>
             </Drawer>
--- frontend/src/Redux/UserRegisterReducer/reducer.js
+++ frontend/src/Redux/UserRegisterReducer/reducer.js
@@ -1,3 +1,5 @@
+import { REGISTER_CONFIRM_PASSWORD, REGISTER_EMAIL, REGISTER_FIRST_NAME, REGISTER_LAST_NAME, REGISTER_PASSWORD } from "./action"
+
 const initialState = {
     firstName: "",
     lastName: "",
@@ -6,14 +8,44 @@ const initialState = {
     confirmPassword: ""
 }
 
-export const reducer = (state = initialState, {type, payload})=>{
+export const reducer = (state = initialState, { type, payload }) => {
 
     switch (type) {
-        case type:
-            
-            break;
-    
+
+        case REGISTER_FIRST_NAME:
+            return {
+                ...state,
+                firstName: payload
+            }
+
+        case REGISTER_LAST_NAME:
+            return {
+                ...state,
+                lastName: payload
+            }
+
+        case REGISTER_EMAIL:
+            return {
+                ...state,
+                email: payload
+            }
+
+        case REGISTER_PASSWORD:
+            return {
+                ...state,
+                password: payload
+            }
+
+        case REGISTER_CONFIRM_PASSWORD:
+            return {
+                ...state,
+                confirmPassword: payload
+            }
+
+
         default:
-            break;
+            return {
+                ...state
+            }
     }
 }
