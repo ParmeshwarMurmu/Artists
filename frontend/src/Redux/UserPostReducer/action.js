b5515aed59aaabfd9f6a7b42c2d639bfd7bd4489
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2023-12-09 21:28:55 +0530

    USER_PROFILE

--- frontend/src/Redux/UserPostReducer/action.js
+++ frontend/src/Redux/UserPostReducer/action.js
@@ -1,5 +1,21 @@
 
 
 export const USER_POST_LOADING = 'USER_POST_LOADING'
-// export const USER_POST_LOADING = 'USER_POST_LOADING'
-// export const USER_POST_LOADING = 'USER_POST_LOADING'
+export const USER_POST_SUCCESS = 'USER_POST_SUCCESS'
+export const USER_POST_ERROR = 'USER_POST_ERROR'
+
+export const userPostLoadingAction = ()=>{
+    return {type: USER_POST_LOADING}
+}
+
+export const userPostSuccessAction = (payload)=>{
+    return {type: USER_POST_SUCCESS, payload}
+}
+
+export const userPostErrorAction = ()=>{
+    return {type: USER_POST_ERROR}
+}
+
+export const getUserPostData = ()=>()=>{
+    
+}
