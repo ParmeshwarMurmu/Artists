d270e9c49cd11e83c62827f08e6bb9c27b8dc680
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2023-12-19 11:07:33 +0530

    UP

--- frontend/src/Variables/Variables.js
+++ frontend/src/Variables/Variables.js
@@ -1,2 +1,2 @@
-// export const APP_URL = 'https://again-art.onrender.com'
-export const APP_URL = 'http://localhost:8000'
+export const APP_URL = 'https://again-art.onrender.com'
+// export const APP_URL = 'http://localhost:8000'
