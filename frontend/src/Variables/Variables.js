2fcf1bb33302cb8aa929ca975725a777a8f7ef83
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2023-12-20 10:16:12 +0530

    Redep

--- frontend/src/Variables/Variables.js
+++ frontend/src/Variables/Variables.js
@@ -1,2 +1,2 @@
-// export const APP_URL = 'https://again-art.onrender.com'
-export const APP_URL = 'http://localhost:8000'
+export const APP_URL = 'https://artists-r5v3.onrender.com'
+// export const APP_URL = 'http://localhost:8000'
