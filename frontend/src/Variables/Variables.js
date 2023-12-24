784f10f9237a8185e06b109337bdc6b307d6886b
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2023-12-24 11:03:35 +0530

    Backend

--- frontend/src/Variables/Variables.js
+++ frontend/src/Variables/Variables.js
@@ -1,2 +1,2 @@
-// export const APP_URL = 'https://artists-6jh5.onrender.com'
-export const APP_URL = 'http://localhost:8000'
+export const APP_URL = 'https://artists-6jh5.onrender.com'
+// export const APP_URL = 'http://localhost:8000'
